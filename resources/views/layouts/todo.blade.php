c3d75f1cf0d76686365795bffbd7d8d1a7c202d8
Giuliano Vallejos Santajuliana <vallejosgvs@gmail.com>
2020-05-29 14:28:21 -0300

    corregida la ruta del navbar

--- resources/views/layouts/todo.blade.php
+++ resources/views/layouts/todo.blade.php
@@ -26,7 +26,7 @@
     <!-- Navigation-->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
         <div class="container">
-            <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="assets/img/3DSolutions.png" alt=""
+            <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="assets/img/3Dsolutions2.png" alt=""
                     width="70" height="50"></a><button class="navbar-toggler navbar-toggler-right" type="button"
                 data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive"
                 aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
